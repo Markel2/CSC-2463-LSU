120f61046eee65822d18e0ea7f859b980477518a
Markel2 <mc1900w@gmail.com>
2024-02-25 16:36:45 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,205 +1,110 @@
-let bug, allBugs, rotationAngles = [0, 90, 180, 270],
-score, gameTime, gameState, timeLeft; 
-
+let bug;
+let bugs = [];
+let bugAnimation;
 
 function preload() {
-  
-  spritesheet = loadSpriteSheet('assets/BugSprite.png');
-
   let animations = {
 
-  aliveUp: {row: 0, frames: 2},
-  aliveLeft: {row: 1, frames: 2},
-  aliveRight: {row: 2, frames: 2},
-  aliveDown: {row: 0, frames: 2},
-  deadUp: {row:0, col: 2, frames: 1},
-  deadLeft: {row:1, col: 2, frames: 1},
-  deadRight: {row:2, col: 2, frames: 1},
-  deadDown: {row:3, col: 2, frames: 1},
+    aliveUp: {row: 0, frames: 2},
+    aliveLeft: {row: 1, frames: 2},
+    aliveRight: {row: 3, frames: 2},
+    aliveDown: {row: 2, frames: 2},
+    deadUp: {row:0, col: 2, frames: 1},
+    deadLeft: {row:1, col: 2, frames: 1},
+    deadRight: {row:3, col: 2, frames: 1},
+    deadDown: {row:2, col: 2, frames: 1},
+      }
+
+    bugs.push(new Bug('assets/BugSpriteFinal.png', animations, 200, 200, 270, 360));
     }
-  class Bug {
-    constructor(spritesheet, animations) {
-      bug = new Sprite(random(50, 750), random(50, 750), 50, 50);
-      this.bug.spriteSheet = spritesheet;
-      bug.addAni('alive', 'assets/BugSprite.png', 13);
-      this.bug.addAnis(animations);
-      bug.anis.frameDelay = 5;
-      bug.rotation = random(rotationAngles);
-      switch(bug.rotation){
-        case 0:
-          bugUp();
-          break;
-        case 90:
-          bugRight();
-          break;
-        case 180:
-          bugDown();
-          break;
-        case 270:
-          bugLeft();
-          break;
-        default:
-          bug.rotation = 0;
-          bugUp();
-          break;
-      }
-
-      function bugRight() {
-        bug.changeAni('aliveRight');
-        bug.vel.x = 1;
-        bug.vel.y = 0;
-      }
-
-      function bugLeft() {
-        bug.changeAni('aliveLeft');
-        bug.vel.x = -1;
-        bug.vel.y = 0;
-      }
-
-      function bugUp() {
-        bug.changeAni('aliveUp');
-        bug.vel.x = 0;
-        bug.vel.y = 1;
-      }
-
-      function bugDown() {
-        bug.changeAni('aliveDown');
-        bug.vel.x = 0;
-        bug.vel.y = -1;
-      }
-
-
-
-    }}
   
-  if (allBugs.size() < 1) {
-    bugSpawner(20);
-  }
-
-}
-
 function setup() {
-  createCanvas(600, 600);
-  allBugs = new Group();
-  score = 0;
-  gameTime = 30;
-  gameState = "start";
-  timeLeft = true;
-
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
+  background(220);
 
-  if(gameState == "start") {
-    startGameNotif();
-    if(mouseIsPressed) {
-      bugSpawner(20);
-      playTime = millis(); 
-      gameState = "play";
-      } else if(gameState == "play") {
-       timer();
-       push();
-       textSize(15);
-       text(`Time remaining: ${gameTime} seconds`, 100, 100);
-       text(`Bugs clicked: ${score}`, 100, 200);
-       pop();
-       allBugs.overlap(allBugs);
+  bugs.forEach((bug) => {
+    if(this.bug.ani = 'aliveUp' && this.bug.mouse.pressing()) {
+      this.bug.changeAni('deadUp')
+      this.bug.vel.x = 0;
+      this.bug.vel.y = 0;
+      score++;
+      this.bug.remove();
+     }
 
-       if(bug.ani = 'aliveUp' && bug.mouse.pressing()) {
-        bug.changeAni('deadUp')
-        bug.vel.x = 0;
-        bug.vel.y = 0;
-        score++;
-        bug.remove();
-       }
+     if(this.bug.ani = 'aliveLeft' && this.bug.mouse.pressing()) {
+      this.bug.changeAni('deadLeft');
+      this.bug.vel.x = 0;
+      this.bug.vel.y = 0;
+      score++;
+      this.bug.remove();
+     }
 
-       if(bug.ani = 'aliveLeft' && bug.mouse.pressing()) {
-        bug.changeAni('deadLeft');
-        bug.vel.x = 0;
-        bug.vel.y = 0;
-        score++;
-        bug.remove();
-       }
+     if(this.bug.ani = 'aliveRight' && this.bug.mouse.pressing()) {
+      this.bug.changeAni('deadRight')
+      this.bug.vel.x = 0;
+      this.bug.vel.y = 0;
+      score++;
+      this.bug.remove();
+     }
 
-       if(bug.ani = 'aliveRight' && bug.mouse.pressing()) {
-        bug.changeAni('deadRight')
-        bug.vel.x = 0;
-        bug.vel.y = 0;
-        score++;
-        bug.remove();
-       }
+     if(this.bug.ani = 'aliveDown' && this.bug.mouse.pressing()) {
+      this.bug.changeAni('deadDown')
+      this.bug.vel.x = 0;
+      this.bug.vel.y = 0;
+      score++;
+      this.bug.remove();}
+  })}
 
-       if(bug.ani = 'aliveDown' && bug.mouse.pressing()) {
-        bug.changeAni('deadDown')
-        bug.vel.x = 0;
-        bug.vel.y = 0;
-        score++;
-        bug.remove();
-       }
 
-       if(score++) {
-        bug.speed = bug.speed + 0.5;
-      }
-  }
+class Bug {
+  constructor(spritesheet, animations, x, y, w, h) {
+    this.bug = new Sprite(x, y, w, h);
+    this.bug.spriteSheet = spritesheet;
+    this.bug.addAnis(animations);
+    this.bug.anis.frameDelay = 5;
+    this.bug.rotation = random(rotationAngles);
+    switch(bug.rotation){
+      case 0:
+        bugUp();
+        break;
+      case 90:
+        bugRight();
+        break;
+      case 180:
+        bugDown();
+        break;
+      case 270:
+        bugLeft();
+        break;
+      default:
+        bug.rotation = 0;
+        bugUp();
+        break;
+    }}
 
-  if(timeLeft = false) {
-    allBugs.remove();
-    gameState = "end";
-  }
-  else if(gameState == 'end') {
-    endGameNotif();
-  }
-  
-}
+     bugRight() {
+      this.bug.changeAni('aliveRight');
+      this.bug.vel.x = 1;
+      this.bug.vel.y = 0;
+    }
 
-function startGameNotif() {
-  push();
-  fill('cyan');
-  stroke(0);
-  strokeWeight(5);
-  rect(width/4, height/4, 250, 250);
-  noStroke();
-  fill(0);
-  textAlign(CENTER);
-  textSize(20);
-  text(`Welcome to the bug squish game!\nUse your mouse or trackpad to click on the bugs.\n
-  You have 30 seconds to click on as many bugs as you can.\n Good luck!`, width/3, width3);
-  pop();
-}
+     bugLeft() {
+      this.bug.changeAni('aliveLeft');
+      this.bug.vel.x = -1;
+      this.bug.vel.y = 0;
+    }
 
-function endGameNotif() {
-  push();
-  fill('cyan');
-  stroke(0);
-  strokeWeight(5);
-  rect(width/4, height/4, 400, 400);
-  noStroke();
-  fill(0);
-  textAlign(CENTER);
-  textSize(20);
-  text(`Time is up! Your score is: ${score}!`, width/3, width3);
-  pop();
-}
+     bugUp() {
+      this.bug.changeAni('aliveUp');
+      this.bug.vel.x = 0;
+      this.bug.vel.y = 1;
+    }
 
-function timer() {
-  gameTime = 30 - int((millis() - playTime) / 1000);
-  if(gameTime > 30) {
-    timeLeft = true;
-  }
-  else if(gameTime > 0) {
-    timeLeft = true;
-  }
-  else if(gameTime = 0){
-    timeLeft = false;
-  }
-}
-
-function bugSpawner(num) {
-  for(i = 0; i < num; i++) {
-    newBug = new Bug('assets/BugSprite.png', animations);
-    allBugs.add(newBug);
-  }
-}
-
-}
+    bugDown() {
+      this.bug.changeAni('aliveDown');
+      this.bug.vel.x = 0;
+      this.bug.vel.y = -1;
+    }}
